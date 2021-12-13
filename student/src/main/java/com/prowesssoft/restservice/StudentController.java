4fb4ac794e1ecab8a3b373586ed1dbb77e0b6505
Pradeep Tallogu <pradeep.tallogu@prowesssoft.com>
2021-12-13 14:17:19 +0530

    Added imports and corrected syntax

--- student/src/main/java/com/prowesssoft/restservice/StudentController.java
+++ student/src/main/java/com/prowesssoft/restservice/StudentController.java
@@ -3,6 +3,7 @@ package com.prowesssoft.restservice;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
--- student/src/main/java/com/prowesssoft/restservice/Students.java
+++ student/src/main/java/com/prowesssoft/restservice/Students.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 public class Students {
 	private List<Student> students;
+
 	public Students() {
 		students = new ArrayList<>();
 	}
-    public List<Student> getStudents() {
-    	students.add(new Student(1, "John Doe", "Computer Science", "66502", "john.doe@school.edu"));
-        return students;
-    }
+
+	public List<Student> getStudents() {
+		students.add(new Student(1, "John Doe", "Computer Science", "66502", "john.doe@school.edu"));
+		return students;
+	}
+}
